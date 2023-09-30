f35af0841cacde7007414080c60dccfc14f614b8
isaacbrds <isaac.brigido@gmail.com>
2023-09-30 16:43:54 -0300

    feat: configuração do usuário

--- eventos/urls.py
+++ eventos/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('eventos/', views.lista_eventos, name='lista_eventos'),
     path('eventos/<int:id>/', views.detalhe_evento, name='detalhe_evento'),
+    #path('eventos/<int:id>/inscricao/', views.inscricao_evento, name='inscricao_evento'),
     path('palestrantes/<int:id>/', views.detalhe_palestrante, name='detalhe_palestrante'),
 ]
--- sistema_eventos/urls.py
+++ sistema_eventos/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('eventos.urls'))
+    path('', include('eventos.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ]
